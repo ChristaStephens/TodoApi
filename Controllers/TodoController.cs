4e6f796fb7a909d3bbc046570fdc344c64b835a6
Christa Stephens <christastephens@quickenloans.com>
2019-08-16 16:14:34 -0400

    created a post method

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -31,7 +31,7 @@ namespace TodoApi.Controllers
             }
         }
 
-        // GET: api/Todo - this is an endpoint
+        // GET: api/Todo - this is an endpoint that gets the entire list
         //this signals a method that responds to a http get request.
         [HttpGet]
         public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
@@ -39,7 +39,7 @@ namespace TodoApi.Controllers
             return await _context.TodoItems.ToListAsync();
         }
 
-        // GET: api/Todo/5 - this is an endpoint
+        // GET: api/Todo/5 - this is an endpoint that gets by an id
         [HttpGet("{id}")]
         //actionresult helps to return the body of the response message
         //it also handles the response code - such as notfound()
@@ -54,6 +54,23 @@ namespace TodoApi.Controllers
 
             return todoItem;
         }
+
+        //Post: api/Todo an endpoint that creates a new item
+        //creates the value of the to-do item from the body of
+        //of the http request
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            //CreatedAtAction return a 201 status code (ok) if successful
+            //also adds a location header to the response
+            //references the GetTodoItem action to create the location header
+            //nameof is used to prevent hard-coding the action name in the CreatedAtAction
+            return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id}, item);
+        }
+
     }
 
 }
