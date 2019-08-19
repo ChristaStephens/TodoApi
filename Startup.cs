40a439f2e5d680d4d43037241b3b73b6482e23b4
Christa Stephens <christastephens@quickenloans.com>
2019-08-19 09:49:07 -0400

    added wwwroot folder with html and js files

--- Startup.cs
+++ Startup.cs
@@ -46,6 +46,9 @@ namespace TodoApi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            //Configures the app to serve static files and enable default file mapping
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseHttpsRedirection();
             app.UseMvc();
