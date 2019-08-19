1ec73d378d6febd8e42b5438d5ccc4c3218dc00f
Christa Stephens <christastephens@quickenloans.com>
2019-08-19 12:07:41 -0400

    added two lines in startup to get index to work

--- Startup.cs
+++ Startup.cs
@@ -46,6 +46,12 @@ namespace TodoApi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // Serve my app-specific default file, if present. Pulled from static file microsoft sight
+            DefaultFilesOptions options = new DefaultFilesOptions();
+            options.DefaultFileNames.Clear();
+            options.DefaultFileNames.Add("index.html");
+
             //Configures the app to serve static files and enable default file mapping
             app.UseDefaultFiles();
             app.UseStaticFiles();
